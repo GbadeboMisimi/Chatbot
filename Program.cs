1e658da846dc8296d700cee9d34df252fc116f17
Oluwamisimi Gbadebo <oluwamisimigbadebo@gmail.com>
2026-06-25 12:32:06 +0100

    swagger fix

--- Program.cs
+++ Program.cs
@@ -125,14 +125,17 @@ builder.Services.AddHttpClient();
 
     var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-    app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
 
     app.UseAuthentication();
 
